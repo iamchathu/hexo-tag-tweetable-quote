5a9a760d9199406bc092951447b5d59e311e216a
Chathu Vishwaijith <4evercreations@gmail.com>
2016-11-29 16:22:09 +0530

    Add basic index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file This is a tweetable quotes tag plugin for the Hexo static site generator.
+ * @copyright Chathu Vishwajith 2015-2016
+ * @author Chathu Vishwajith
+ * @license MIT
+ */
+hexo.extend.tag.register('tweetable-tag', function(args) {
+
+  // TODO: Implementation 
+});
