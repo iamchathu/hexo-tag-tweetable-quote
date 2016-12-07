50e2e65c8542298b36cabd944e216610e61ca095
Chathu Vishwajith <4evercreations@gmail.com>
2016-12-07 20:36:44 +0530

    Add config variables

--- index.js
+++ index.js
@@ -4,9 +4,23 @@
  * @author Chathu Vishwajith
  * @license MIT
  */
+
+ // Strict mode.
+'use strict';
+
+// Package modules.
+const assign = require('object-assign');
 const path = require('path');
 const nunjucks = require('nunjucks');
 const querystring = require('querystring');
+
+// Configuration
+hexo.config.tweetableQuote = assign({
+  quote_font_color    : '#258fb8',
+  quote_font_size : '1.7em',
+  link_font_color : '#6e7b8d',
+  link_font_size : '1.0em'
+}, hexo.config.tweetableQuote);
 
 nunjucks.configure(__dirname, {watch: false});
 
@@ -16,7 +30,15 @@ hexo.extend.tag.register('tweetableQuote', function(args) {
 	let author = args[1];
 	const url = 'https://twitter.com/intent/tweet?'+querystring.stringify({"text":quote+" - "+author,"via":"iamchathu"});
 
-	const data = {"quote": quote, "author": author,"url":url};
+	const data = {
+		"quote": quote,
+		"author": author,
+		"url":url,
+		"quote_font_color":hexo.config.tweetableQuote.quote_font_color,
+		"quote_font_size":hexo.config.tweetableQuote.quote_font_size,
+		"link_font_size":hexo.config.tweetableQuote.link_font_size,
+		"link_font_color":hexo.config.tweetableQuote.link_font_color,
+	};
 
 	return new Promise(function (resolve, reject) {
     	nunjucks.render('tweetable-quote.njk', data, function (err, res) {
