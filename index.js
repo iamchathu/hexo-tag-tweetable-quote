f8bd72f0b174a6e857c1ff7f7ed2fa0cb59297cf
Chathu Vishwajith <4evercreations@gmail.com>
2016-12-08 11:42:40 +0530

    Test URL parameter

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ hexo.extend.tag.register('tweetableQuote', function(args) {
 
 	let quote = args[0];
 	let author = args[1];
-	const tweetURL = 'https://twitter.com/intent/tweet?'+querystring.stringify({"text":quote+" - "+author,"via":"iamchathu","url":url});
+	const tweetURL = 'https://twitter.com/intent/tweet?'+querystring.stringify({"text":quote+" - "+author,"via":"iamchathu","url":"{{% url %}}"});
 
 	const data = {
 		"quote": quote,
